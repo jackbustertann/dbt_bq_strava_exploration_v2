15ba668d24d05835af09fbba4a27a30e5e933a7a
Jack Tann <jackbustertann@icloud.com>
2022-05-15 16:05:14 +0000

    adding tests to staging tables + source freshness

--- models/staging/stg_activities.sql
+++ models/staging/stg_activities.sql
@@ -1,10 +1,15 @@
-SELECT
-  * EXCEPT (start_date, start_date_local), 
-  PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', start_date) AS start_date, 
-  PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', start_date_local) AS start_date_local,
-  CAST(SPLIT(TRIM(start_latlng, '[]'), ',')[OFFSET(0)] AS FLOAT64) AS start_lat,
-  CAST(SPLIT(TRIM(start_latlng, '[]'), ',')[OFFSET(1)] AS FLOAT64) AS start_lng,
-  CAST(SPLIT(TRIM(end_latlng, '[]'), ',')[OFFSET(0)] AS FLOAT64) AS end_lat,
-  CAST(SPLIT(TRIM(end_latlng, '[]'), ',')[OFFSET(1)] AS FLOAT64) AS end_lng
-FROM
-  {{ source('strava_dev', 'activities') }}
+WITH activities AS (
+  SELECT
+    * EXCEPT (start_date, start_date_local, last_updated), 
+    PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', start_date) AS start_date, 
+    PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', start_date_local) AS start_date_local,
+    PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', last_updated) AS last_updated,
+    CAST(SPLIT(TRIM(start_latlng, '[]'), ',')[OFFSET(0)] AS FLOAT64) AS start_lat,
+    CAST(SPLIT(TRIM(start_latlng, '[]'), ',')[OFFSET(1)] AS FLOAT64) AS start_lng,
+    CAST(SPLIT(TRIM(end_latlng, '[]'), ',')[OFFSET(0)] AS FLOAT64) AS end_lat,
+    CAST(SPLIT(TRIM(end_latlng, '[]'), ',')[OFFSET(1)] AS FLOAT64) AS end_lng
+  FROM
+    {{ source('strava_dev', 'activities') }}
+)
+
+SELECT * FROM activities
--- models/staging/stg_activity_zones.sql
+++ models/staging/stg_activity_zones.sql
@@ -1,4 +1,20 @@
-SELECT
-  * 
-FROM
-  {{ source('strava_dev', 'activity_zones') }}
+WITH activity_zones AS (
+  SELECT
+    * EXCEPT (last_updated), 
+    RANK() OVER(PARTITION BY activity_id, type ORDER BY min, max) AS zone_index,
+    CASE 
+      WHEN type = 'heartrate' THEN 0
+      WHEN type = 'pace' THEN 1
+      WHEN type = 'power' THEN 2
+      ELSE -1
+    END AS type_index,
+    PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', last_updated) AS last_updated
+  FROM
+    {{ source('strava_dev', 'activity_zones') }}
+)
+
+SELECT 
+  *,
+  activity_id || CAST(type_index AS STRING) || FORMAT('%02d', zone_index) AS id
+FROM 
+  activity_zones
