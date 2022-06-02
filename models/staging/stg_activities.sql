1f7356e22757daa90064294d677544b9ce281c8a
Jack Tann <jackbustertann@icloud.com>
2022-06-02 14:55:34 +0000

    adding jinja, macros and packages

--- models/staging/stg_activities.sql
+++ models/staging/stg_activities.sql
@@ -1,15 +1,18 @@
+{%- set date_cols = ['start_date', 'start_date_local', 'last_updated'] -%}
+
 WITH activities AS (
   SELECT
     * EXCEPT (start_date, start_date_local, last_updated), 
-    PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', start_date) AS start_date, 
-    PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', start_date_local) AS start_date_local,
-    PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', last_updated) AS last_updated,
+    {% for date_col in date_cols -%}
+    PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', {{date_col}}) AS {{date_col}},
+    {% endfor -%}
     CAST(SPLIT(TRIM(start_latlng, '[]'), ',')[OFFSET(0)] AS FLOAT64) AS start_lat,
     CAST(SPLIT(TRIM(start_latlng, '[]'), ',')[OFFSET(1)] AS FLOAT64) AS start_lng,
     CAST(SPLIT(TRIM(end_latlng, '[]'), ',')[OFFSET(0)] AS FLOAT64) AS end_lat,
     CAST(SPLIT(TRIM(end_latlng, '[]'), ',')[OFFSET(1)] AS FLOAT64) AS end_lng
   FROM
     {{ source('strava_dev', 'activities') }}
+  {# {{ limit_dev() }} #}
 )
 
 SELECT * FROM activities
