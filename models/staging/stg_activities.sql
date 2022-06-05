302b9d0755ec3775890c73fd954a319d06132b8c
Jack Tann <jackbustertann@icloud.com>
2022-06-05 12:26:34 +0000

    adding snapshots

--- models/staging/stg_activities.sql
+++ models/staging/stg_activities.sql
@@ -1,18 +1,34 @@
-{%- set date_cols = ['start_date', 'start_date_local', 'last_updated'] -%}
+{{
+  config(
+    materialized = 'incremental',
+    unique_key = 'id'
+  )
+}}
 
+/* setting lineage */
 WITH activities AS (
+  SELECT * 
+  FROM {{ source('strava_dev', 'activities') }}
+  {% if is_incremental() %}
+  WHERE TIMESTAMP_ADD(PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', start_date), INTERVAL 3 DAY) > (SELECT MAX(start_date) FROM {{ this }})
+  {% endif %} 
+)
+
+/* casting data types and defining new columns */
+{% set date_cols = ['start_date', 'start_date_local', 'last_updated'] -%}
+
+, activities_final AS (
   SELECT
     * EXCEPT (start_date, start_date_local, last_updated), 
     {% for date_col in date_cols -%}
     PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', {{date_col}}) AS {{date_col}},
     {% endfor -%}
-    CAST(SPLIT(TRIM(start_latlng, '[]'), ',')[OFFSET(0)] AS FLOAT64) AS start_lat,
-    CAST(SPLIT(TRIM(start_latlng, '[]'), ',')[OFFSET(1)] AS FLOAT64) AS start_lng,
-    CAST(SPLIT(TRIM(end_latlng, '[]'), ',')[OFFSET(0)] AS FLOAT64) AS end_lat,
-    CAST(SPLIT(TRIM(end_latlng, '[]'), ',')[OFFSET(1)] AS FLOAT64) AS end_lng
+    SAFE_CAST(IF(LENGTH(TRIM(start_latlng, '[]')) > 0, SPLIT(TRIM(start_latlng, '[]'), ',')[OFFSET(0)], NULL) AS FLOAT64) AS start_lat,
+    SAFE_CAST(IF(LENGTH(TRIM(start_latlng, '[]')) > 0, SPLIT(TRIM(start_latlng, '[]'), ',')[OFFSET(1)], NULL) AS FLOAT64) AS start_lng,
+    SAFE_CAST(IF(LENGTH(TRIM(end_latlng, '[]')) > 0, SPLIT(TRIM(end_latlng, '[]'), ',')[OFFSET(0)], NULL) AS FLOAT64) AS end_lat,
+    SAFE_CAST(IF(LENGTH(TRIM(end_latlng, '[]')) > 0, SPLIT(TRIM(end_latlng, '[]'), ',')[OFFSET(1)], NULL) AS FLOAT64) AS end_lng
   FROM
-    {{ source('strava_dev', 'activities') }}
-  {# {{ limit_dev() }} #}
+    activities
 )
 
-SELECT * FROM activities
+SELECT * FROM activities_final
---
+++ snapshots/snp_activities.sql
@@ -0,0 +1,16 @@
+{% snapshot snp_activities %}
+
+{{
+    config(
+      target_database='strava-exploration-v2',
+      target_schema='strava_snapshots',
+      unique_key='id',
+
+      strategy='timestamp',
+      updated_at='last_updated',
+    )
+}}
+
+select * from {{ source('strava_dev', 'activities') }}
+
+{% endsnapshot %}
