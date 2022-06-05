cdda562643725f4086ed3527fb46e5ed06c3f1de
Jack Tann <jackbustertann@icloud.com>
2022-06-05 14:18:36 +0000

    adding analyses and seeds

---
+++ macros/generate_schema_name.sql
@@ -0,0 +1,18 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- elif node.resource_type == "seed" -%}
+        
+        {{ custom_schema_name | trim }}
+
+    {%- else -%}
+        
+        {{ default_schema }}_{{ custom_schema_name | trim }}
+        
+    {%- endif -%}
+
+{%- endmacro %}
